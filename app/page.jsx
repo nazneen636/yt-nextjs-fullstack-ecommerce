28d0510664db27f8c55031239b261f58968f8a38
nazneen636 <nazneennahar636@gmail.com>
2025-09-16 13:37:04 +0600

    scalfholding

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+const page = () => {
+  return (
+    <div>
+      <Button>Like</Button>
+    </div>
+  );
+};
+
+export default page;
